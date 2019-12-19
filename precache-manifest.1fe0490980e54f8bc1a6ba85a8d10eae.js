cfeaf4695ba23723b22a7bdf658095f26e28b7bc
Ziv <ziv.israeli01@gmail.com>
2019-12-18 16:50:32 -0800

    Updates

---
+++ precache-manifest.1fe0490980e54f8bc1a6ba85a8d10eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d423a424b9435520b8c96bce4df8cbb5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3fa5d81c5a3071cf8c42",
+    "url": "/robofriends/static/css/main.3014d7f5.chunk.css"
+  },
+  {
+    "revision": "2c385a86eedf33318b0e",
+    "url": "/robofriends/static/js/2.56484c12.chunk.js"
+  },
+  {
+    "revision": "3fa5d81c5a3071cf8c42",
+    "url": "/robofriends/static/js/main.fbc2ccd0.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
